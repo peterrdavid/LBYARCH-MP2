f383328c4a5e007e06638663c8fb65efd12447b1
Peter David <davidpeterjan@gmail.com>
2024-11-26 20:04:37 +0800

    declared variables

---
+++ saxpy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;                  // vector size
+    int i;                  // index
+    float *X, *Y;           // pointers to vectors X and Y (input vectors)
+    float *Z_c, *Z_asm;     // pointers to vectors Z for C and x86-64 assembly (result vectors)
+    float A;                // scalar value
+
+    return 0;
+}
