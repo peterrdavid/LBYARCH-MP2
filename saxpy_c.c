f19ce23700b09e353780dafbe077199cf80c259d
Peter David <davidpeterjan@gmail.com>
2024-12-02 20:16:41 +0800

    fixed formatting

--- saxpy_c.c
+++ saxpy_c.c
@@ -14,26 +14,33 @@ void saxpy_c(float *X, float *Y, float *Z, int n, float A) {
 }
 
 int main() {
-  int i; // index
-  int j; // index
-  float A;            // scalar value
-  float *X, *Y;       // pointers to vectors X and Y (input vectors)
-  float *Z_c, *Z_asm; // pointers to vectors Z for C and x86-64 assembly (result vectors)
-  int vector_sizes[] = {1 << 20, 1 << 24, 1 << 30}; // may want to change 30 to a smaller number if machine cannot handle
+  int i;  
+  int j;
+  float A;         
+  float *X, *Y;       
+  float *Z_c, *Z_asm;
+  int vector_sizes[] = {1 << 20, 1 << 24, 1 << 28}; 
   clock_t start_time, end_time;
   long durations[30];
   long durations_sum = 0;
   int c_avg_runtimes[3];
   int asm_avg_runtimes[3];
 
-  srand(time(NULL)); // initiate random number generation
+  // Initiate random number generation
+  srand(time(NULL)); 
 
+
+  // Loop for the 3 vector sizes (2^20, 2^24, 2^28)
   for (i = 0; i < 3; i++) {
-    A = (rand() % 10) + 1; // assign random value to A
+    A = (rand() % 10) + 1; // Assign random value to A
 
-    printf("\n##### RUN NUMBER %d #####\n\n", i + 1);
 
-    printf("VECTOR SIZE = %d \n\n", vector_sizes[i]);
+    printf("_________________________________\n");
+    printf("\n######### RUN NUMBER %d #########\n", i + 1);
+
+    printf("\nVECTOR SIZE = %d \n", vector_sizes[i]);
+    
+    printf("_________________________________\n");
 
     // allocate memory
     X = (float *)malloc(vector_sizes[i] * sizeof(float));
@@ -70,16 +77,17 @@ int main() {
     c_avg_runtimes[i] = durations_sum / 30;
 
     // C Output
-    printf("First 10 Results for C kernel:\n");
+    printf("C KERNEL\n");
+    printf("First 10 Results:\n");
     for (j = 0; j < 10; j++) {
       if (Z_c[j] == 0) {
         break;
       } else {
-        printf("C: Z[%d] = %.2f\n", j, Z_c[j]);
+        printf("Z[%d] = %.2f\n", j, Z_c[j]);
       }
     }
 
-    printf("Duration for C kernel = %lu \n\n", c_avg_runtimes[i]);
+    printf("Duration: %lu \n\n", c_avg_runtimes[i]);
 
     // call x86-64 assembly kernel and time each execution, storing into durations
     for (j = 0; j < 30; j++) {
@@ -96,23 +104,24 @@ int main() {
     asm_avg_runtimes[i] = durations_sum / 30;
 
     // Assembly Output
-    printf("First 10 Results for Assembly kernel:\n");
+    printf("ASSEMBLY KERNEL\n");
+    printf("First 10 Results:\n");
     for (j = 0; j < 10; j++) {
       if (Z_asm[j] == 0) {
         break;
       } else {
-        printf("C: Z[%d] = %.2f\n", j, Z_asm[j]);
+        printf("Z[%d] = %.2f\n", j, Z_asm[j]);
       }
     }
 
     
-    printf("Duration for Assembly kernel = %lu \n\n", asm_avg_runtimes[i]);
+    printf("Duration: %lu \n\n", asm_avg_runtimes[i]);
 
     int unequal = memcmp(Z_c, Z_asm, sizeof(Z_c));
     if(!unequal)
-        printf("Assembly and C results match\n\n");
+        printf("Assembly and C results match.\n");
     else
-        printf("Assembly and C results do not match\n\n");
+        printf("Assembly and C results do not match.\n");
 
     // free memory
     free(X);
